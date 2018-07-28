27a026b77c4353b425dc91390c92c2a88b0182c2
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-28 13:27:13 -0500

    asdf

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/"));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.get("/:app", function(req, res) {
+  let app_folder = req.params.app;
+  res.sendFile(path.join(__dirname + `/apps/${app_folder}/dist/index.html`));
 });
 
-app.get("/php", function(req, res) {
-  res.sendFile(path.join(__dirname + "myphp.php"));
+app.post("/api/email/", function(req, res) {
+  var api_content = null;
+  api_content = {
+    api: "email",
+    response: "Email Api Request"
+  };
+  var api_out = JSON.stringify(api_content);
+  res.send();
 });
 
 // Start the app by listening on the default Heroku port
