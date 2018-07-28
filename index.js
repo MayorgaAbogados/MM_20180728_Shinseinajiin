9588b80246fee02a6c7546b47ca6418e67b8f8a7
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-28 13:57:20 -0500

    zxc

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/"));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + `index.html`));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 /*
