d0f0773fc0b0ea9e06a4e95d7612362adbc153cc
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-08-02 10:41:38 -0500

    hghg

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# crawler-sigep
-Crawler/Bot para recuperar la información del SIGEP y estandarizarla en una base de datos.
+## 
+README
--- server/routes/web.php
+++ server/routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('index/index');
 });
 
-Route::get('/index', function () {
+Route::get('about', function () {
     return view('index/index');
 });
