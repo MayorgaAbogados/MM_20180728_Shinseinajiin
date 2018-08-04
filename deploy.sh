8292117f2a1fcd04bfac6380362fde01e8b0fcfc
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2018-08-04 13:22:46 -0500

    Create deploy.sh
    Parar ejecutarlo :
    
    # sed -i -e 's/\r$//' 
    # chmod 755 hello_world
    # ./deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Deploy From Git
+# sed -i -e 's/\r$//' 
+# chmod 755 hello_world
+# ./deploy.sh
+echo " "
+echo " "
+echo " "
+echo "-----------------------------------------------"
+echo "**Deploy From Github**"
+echo ".."
+echo ".."
+echo ".. :: Pulling GitHub Repo   "
+echo ".."
+echo ".."
+git pull https://github.com/MayorgaAbogados/MM_20180728_Shinseinajiin.git
+echo ".."
+echo ".."
+echo ".. :: Update Github Repo "
+echo ".."
+echo ".."
+echo "-----------------------------------------------"
+echo " "
+echo " "
+echo " "
