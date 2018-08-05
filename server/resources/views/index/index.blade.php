67302be3a2c4e4fc0579b51d9fdaab1089d4b0c3
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-08-05 14:10:55 -0500

    upload

--- server/resources/views/index/index.blade.php
+++ server/resources/views/index/index.blade.php
@@ -1,50 +1,58 @@
 <!doctype html>
+
+
+
+
+@php
+$meta = new \stdClass();
+$meta->title = "Soluciones Legales";
+$meta->keywords = "abogados, asesoria legal, litigio, demandas, derecho aduanero";
+$meta->description = "Areas de Práctica:  DERECHO ADUANERO  DERECHO CAMBIARIO , DERECHO TRIBUTARIO  DERECHO COMERCIAL Y SOCIETARIO , DERECHO PENAL  DERECHO LABORAL CORPORATIVO 
+DERECHO AMBIENTAL, CONTRATACIÓN PÚBLICA , DERECHO DE FAMILIA , DERECHO DE LAS TELECOMUNICACIONES ";
+$meta->author = "www.jlmayorga.com.co";
+$meta->data = [
+    'title' => $meta->title, 
+    'keywords' => $meta->keywords,
+    'author' => $meta->author,
+    'description' => $meta->description
+];
+@endphp
+
+
+
+
+
+
 <html lang="{{ app()->getLocale() }}">
+
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>&lt jl &gtcd | Engennering, Development, Science and more.</title>
-
+       @include('meta-tags', $meta->data)
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!-- Styles -->
       <link rel="stylesheet" href="css/app.css">
       <link rel="stylesheet" href="css/main.css">
+      <style>
+        .vertical-center {
+            min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+            min-height: 100vh; /* These two lines are counted as one :-)       */
+
+            display: flex;
+            align-items: center;
+        }
+      </style>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-               <span>&lt</span> jl <span>&gt</span>
-                </div>
+        <div class="container vertical-center">
+        
+            <h1 style="text-align:center;margin:0 auto;">Mayorga Abogados</h1>
+            <div id="app"></div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">developer</a>
-                    <a href="https://laracasts.com">A.I.</a>
-                    <a href="https://laravel-news.com">control systems</a>
-                    <a href="https://forge.laravel.com">robotic</a>
-                    <a href="https://github.com/laravel/laravel">portfolio</a>
-                </div>
-                <div id="app">
-                        <example-component></example-component>
-
-                </div>
-                
-            </div>
         </div>
+
+
         <script src="js/app.js"></script>
     </body>
 </html>
